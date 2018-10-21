e443e55741b00bc1875c101702055ccf8ec80f72
Joseph <joseph.miclatbourne@gmail.com>
2018-10-21 19:04:34 +0800

    Added trigger event

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+
+	public float speed;
+	public Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		var horizontalMove = Input.GetAxis("Horizontal");
+		var verticalMove = Input.GetAxis("Vertical");
+		var movement = new Vector3(horizontalMove, 0.0f, verticalMove);
+		
+		rb.AddForce(movement * speed);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pick Up"))
+		{
+			other.gameObject.SetActive(false);
+//			Destroy(other.gameObject);			
+		}
+	}
+}
