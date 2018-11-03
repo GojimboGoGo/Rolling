8830c7dc2ebda8159af4dde4cf1dd930afbd9d7f
Joseph <joseph.miclatbourne@gmail.com>
2018-11-03 14:09:48 +0800

    Added text UI elements

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player_Controller : MonoBehaviour
 {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
+	
 	public Rigidbody rb;
+	private int count;
 
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		setCountText();
+		winText.text = "";
 	}
 
 	private void FixedUpdate()
@@ -22,10 +30,19 @@ public class Player_Controller : MonoBehaviour
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject.CompareTag("Pick Up"))
+		if (!other.gameObject.CompareTag("Pick Up")) return;
+		
+		other.gameObject.SetActive(false);
+		count++;
+		setCountText();
+	}
+
+	private void setCountText()
+	{
+		countText.text = string.Format("Count: {0}", count);
+		if (count >= 12)
 		{
-			other.gameObject.SetActive(false);
-//			Destroy(other.gameObject);			
+			winText.text = "You Win!";
 		}
 	}
 }
